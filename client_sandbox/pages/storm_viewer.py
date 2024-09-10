b297aeaeed700a207dfb2f3fa16fc0fc0b2dd1c1
zherbz <zherbz32@gmail.com>
2024-09-10 21:55:29 +0000

    debugged heatmaps and moved widgets to sidebar

--- client_sandbox/pages/storm_viewer.py
+++ client_sandbox/pages/storm_viewer.py
@@ -32,46 +32,48 @@ def app():
         }
     )
 
-    search_col1, search_col2 = st.columns(2)
+    # create a sidebar for all of the input filters
+    st.sidebar.title("Input Search Filters")
 
-    with search_col1:
-        sieve1 = df.copy()
-        st.session_state["realization"] = st.multiselect("Select by Realization",
-                                                         sieve1["Realization"].unique(),
-                                                         default=list(sieve1["Realization"].unique())[0])
-        sieve1 = sieve1[sieve1["Realization"].isin(st.session_state["realization"])]
-        st.session_state["block"] = st.slider("Select by Block Range",
-                                              min_value=sieve1["Block"].min(),
-                                              max_value=sieve1["Block"].max(),
-                                              value=(122, 255))
-        sieve1 = sieve1[(sieve1["Block"] >= st.session_state["block"][0]) & (sieve1["Block"] <= st.session_state["block"][1])]
+    # search_col1, search_col2 = st.columns(2)
 
-        st.session_state["search_id"] = st.multiselect("Search by ID", sieve1["ID"].unique())
-        if len(st.session_state["search_id"]) > 0:
-            st.write("filtering by search_id")
-            sieve1 = sieve1[sieve1["ID"].isin(st.session_state["search_id"])]
+    # with search_col1:
+    sieve1 = df.copy()
+    st.session_state["realization"] = st.sidebar.multiselect("Select by Realization",
+                                                        sieve1["Realization"].unique(),
+                                                        default=list(sieve1["Realization"].unique())[0])
+    sieve1 = sieve1[sieve1["Realization"].isin(st.session_state["realization"])]
+    st.session_state["block"] = st.sidebar.slider("Select by Block Range",
+                                            min_value=sieve1["Block"].min(),
+                                            max_value=sieve1["Block"].max(),
+                                            value=(122, 255))
+    sieve1 = sieve1[(sieve1["Block"] >= st.session_state["block"][0]) & (sieve1["Block"] <= st.session_state["block"][1])]
 
-    with search_col2:
-        #sieve2 = sieve1.copy()
-        if sieve1["Max Precip (in)"].min() != sieve1["Max Precip (in)"].max():
-            st.session_state["max_precip"] = st.slider("Search by Max Precipitation (inches)",
-                                                    min_value=sieve1["Max Precip (in)"].min(),
-                                                    max_value=sieve1["Max Precip (in)"].max(),
-                                                    value=sieve1["Max Precip (in)"].mean(),
-                                                    step=0.1)
-            sieve1 = sieve1[sieve1["Max Precip (in)"] >= st.session_state["max_precip"]]
+    st.session_state["search_id"] = st.sidebar.multiselect("Search by ID", sieve1["ID"].unique())
+    if len(st.session_state["search_id"]) > 0:
+        st.write("filtering by search_id")
+        sieve1 = sieve1[sieve1["ID"].isin(st.session_state["search_id"])]
 
-        st.session_state["storm_season"] = st.multiselect("Search for Seasonal Storms",
-                                                        ["All", "spring", "summer", "fall", "winter"],
-                                                        default=["All"])
-        if "All" not in st.session_state["storm_season"]:
-            sieve1 = sieve1[sieve1["Season"].isin(st.session_state["storm_season"])]
+    # with search_col2:
+    if sieve1["Max Precip (in)"].min() != sieve1["Max Precip (in)"].max():
+        st.session_state["max_precip"] = st.sidebar.slider("Search by Max Precipitation (inches)",
+                                                min_value=sieve1["Max Precip (in)"].min(),
+                                                max_value=sieve1["Max Precip (in)"].max(),
+                                                value=sieve1["Max Precip (in)"].mean(),
+                                                step=0.1)
+        sieve1 = sieve1[sieve1["Max Precip (in)"] >= st.session_state["max_precip"]]
 
-        st.session_state["storm_date"] = st.multiselect("Search by Storm Date",
-                                                        ["All", *sieve1["Date"].unique()],
-                                                        default=["All"])
-        if "All" not in st.session_state["storm_date"]:
-            sieve1 = sieve1[sieve1["Date"].isin(st.session_state["storm_date"])]
+    st.session_state["storm_season"] = st.sidebar.multiselect("Search for Seasonal Storms",
+                                                    ["All", "spring", "summer", "fall", "winter"],
+                                                    default=["All"])
+    if "All" not in st.session_state["storm_season"]:
+        sieve1 = sieve1[sieve1["Season"].isin(st.session_state["storm_season"])]
+
+    st.session_state["storm_date"] = st.sidebar.multiselect("Search by Storm Date",
+                                                    ["All", *sieve1["Date"].unique()],
+                                                    default=["All"])
+    if "All" not in st.session_state["storm_date"]:
+        sieve1 = sieve1[sieve1["Date"].isin(st.session_state["storm_date"])]
 
     st.write("Filtered Dataset")
     st.dataframe(sieve1)
@@ -89,9 +91,9 @@ def app():
 
         # initialize the maps
         m1 = folium.Map(location=[37.75153, -80.94911], zoom_start=6)
-        folium.GeoJson(f"{st.stac_url}/collections/Kanawha-0505-R001/items/R001-E2044").add_to(m1)
+        folium.GeoJson(f"{st.stac_url}/collections/Kanawha-R01/items/E005125").add_to(m1)
         m2 = folium.Map(location=[37.75153, -80.94911], zoom_start=6)
-        folium.GeoJson(f"{st.stac_url}/collections/Kanawha-0505-R001/items/R001-E2044").add_to(m2)
+        folium.GeoJson(f"{st.stac_url}/collections/Kanawha-R01/items/E005125").add_to(m2)
         
         # create a heatmap for the historic storm center points
         folium.plugins.HeatMap(data=historic_gdf["geometry"].apply(lambda pt: [pt.y, pt.x]).tolist()).add_to(m1)
@@ -101,11 +103,10 @@ def app():
         col3, col4 = st.columns(2)
         with col3:
             st.write("Historic Storm Centers Heatmap")
-            st_folium(m1, width=700, height=700)
+            st_folium(m1, width=700, height=700, key="m1")
         with col4:
             st.write("SST Storm Centers Heatmap")
-            st_folium(m2, width=700, height=700)
-
+            st_folium(m2, width=700, height=700, key="m2")
 
 if __name__ == "__main__":
     app()
